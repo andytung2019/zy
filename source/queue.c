a8d26a7f6d11a576de55bab11963a47daf4e31ca
andytung2019 <1512696580@qq.com>
2019-03-12 22:24:11 +0800

    update queue

--- source/queue.c
+++ source/queue.c
@@ -2,43 +2,81 @@
 #include <queue.h>
 
 
-static t_queue *p = null;
-static int start = 0;
-static int end = 0;
-static int cur = 0;
+t_queue* init_queue(void) {
 
-int init_queue(t_queue *p)
-  if( null == p){
-		p = (t_queue *)malloc(LEN_QUEUE*sizeof(t_queue)); 
-    }
-   
+  t_queue *p = null;
+
+	p = (t_queue *)malloc(sizeof(t_queue)); 
+ 
    if (null == p ) {
 		printf(" malloc queue failed!\n");
-		return -1;	
+		return null;	
    }
-     
   
+  // init the mem in queue; 
+  memset((unsigned char*)p->block, 0, NUM_BLOCK*BLOCK_RECORD*LEN_RECORD); //mem total len: 16*100*48
+  p->len = 0;
+  p->head = 0;
+  p->tail = 0;
+  return p;
 }
 
 
-int en_queue(t_queue *q , t_queue data) {
+int en_queue(t_queue *q , t_block data) {
+  t_block *pBlock = null;
 
+  if (null == q) {
+    return -1;
+  }
+  
+  // the queue full 
+  if (q->len == NUM_BLOCK) {
+    return -2;
+  }
 
+  if (q->len == 0) {
+    memecpy(q->block[q->head], data, sizeof(t_block));
+    q->tail = 1;
+    q->len = q->len + 1;
+    return q->len;  
+  } 
+  
+  // queue is not empty and not full
+  pBlock = q->tail;
+  memcpy((unsigned char*)q->tail, data, sizeof(t_block));
+  q->len = q->len + 1;
+  q->tail = q->block[q->len];
+  return q->len;
+   
 
 }
 
-int  de_queue(t_queue *q, t_queue *recv) {
+int  de_queue(t_queue *q, t_block *recv) {
+   if( null == q) {
+     return -1;
+   }
+   if( q->len == 0) {
+    return 0;
+   }
 
+   memcpy(recv, q->head, sizeof(t_block));
+      
+      //head is the end of array;
+
+   q->head = 
 
 }
 
-int clear_queue(t_queue *q ) {
-
-
-}
 
 
 int free_queue(t_queue *q) {
+  if ( null != q) {
+    memset((unsigned char*)p->block, 0, NUM_BLOCK*BLOCK_RECORD*LEN_RECORD); //mem total len: 16*100*48
+    free(q);
+  }
+
+  return 0;
+}
 
 
 
--- source/queue.h
+++ source/queue.h
@@ -1,14 +1,26 @@
 
 
 #define LEN_RECORD 48
-#define LEN_QUEUE 100
-
+#define BLOCK_RECORD 100
+#define NUM_BLOCK 16
+#define  LEN_ERROR 6
 
 typdef struct {
-	int amount_record;
+	int amount;
  	unsigned char record[LEN_RECORD];
-	int error;
-} t_queue;
+	unsigned char error[LEN_ERROR];
+} t_node;
 
+
+typedef struct {
+	t_node node[BLOCK_RECORD]; //BLOCK_RECOD*node is  a block;
+} t_block;
+
+typedef struct{
+ unsigned char head;
+ unsigned char tail;
+ t_block block[NUM_BLOCK]; // our queue has NUM_BLOCK  blocks; 
+ int len;
+} t_queue;
 
 
